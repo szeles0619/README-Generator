85dfd300fe4996f5d78a2fff3af2098553cd93ef
Istvan <szeles0619@gmail.com>
2023-02-28 13:44:04 +0000

    Added questions to index.js file

--- index.js
+++ index.js
@@ -4,9 +4,52 @@ const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
-const questions = [
-
-];
+const questions = [{
+    type: "input",
+    message: "What's the title of the project?",
+    name: "Title",
+}, {
+    type: "input",
+    message: "Give some details about your project. (i.e. What was your motivation?, What problem does it solve?, Why did you build this project?)",
+    name: "Description",
+}, {
+    type: "input",
+    message: "Table of contents",
+    name: "Table of contents",
+}, {
+    type: "input",
+    message: "How can I install the application?",
+    name: "Installation",
+}, {
+    type: "input",
+    message: "How can I use the application?",
+    name: "Usage",
+}, {
+    type: "input",
+    message: "What license is being used?",
+    name: "License",
+    //choices: ['\tMIT', '\tGPL-3-0', '\tapache-2-0', '\tBSD-3-clause', '\tISC', '\t0bsd'],
+}, {
+    type: "input",
+    message: "Contributors for your project",
+    name: "Contributors",
+}, {
+    type: "input",
+    message: "How can I test the application?",
+    name: "Test",
+}, {
+    type: "input",
+    message: "Who shall I contact with for inquires?",
+    name: "Questions",
+}, {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "Username",
+}, {
+    type: "input",
+    message: "What is your email address?",
+    name: "Email",
+}];
 
 // function to write README file
 function writeToFile(fileName, data) {
